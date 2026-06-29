d45b35cacf71109bec579bca9ddaf7caf0b26762
IMays <idrismays@gmail.com>
2026-06-29 00:05:11 -0400

    Center footer copyright text

--- components/footer.tsx
+++ components/footer.tsx
@@ -71,7 +71,7 @@ export function Footer() {
         </div>
       </div>
       <div className="border-t border-white/10 py-5">
-        <div className="container-pad text-sm text-slate-400">
+        <div className="container-pad text-center text-sm text-slate-400">
           Copyright {new Date().getFullYear()} 365INO, CO. All rights reserved.
         </div>
       </div>
